cdfc067747787b41f46448b175d2b59c07075362
mrasyadc <mrcrasyad26@gmail.com>
2024-01-23 05:59:08 +0700

    code refactor

--- image_entropy_equalization.py
+++ image_entropy_equalization.py
@@ -1,120 +1,122 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin, OneToOneFeatureMixin
 
+
 class ImageEntropyEqualization(OneToOneFeatureMixin, TransformerMixin, BaseEstimator):
+    _parameter_constraints: dict = {
+        "method": ["entropy", "class", "all"],
+        "entropy_value": [2, 3, 4, 5, 6, 7, 8, 9],
+        "label_value": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
 
-  _parameter_constraints: dict = {
-    "method": ["entropy", "class", "all"],
-    "entropy_value": [1, 2, 3, 4, 5, 6, 7, 8],
-    "label_value": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  }
-  def __init__(self, method:str=None, entropy_value:int=None, label_value:int=None):
-    self.pixel_set = []
-    self.method = method
-    self.entropy_value = entropy_value
-    self.label_value = label_value
-     
+    def __init__(self, *, method: str = None, entropy_value: int = None, label_value: int = None):
+        allowed_methods = self._parameter_constraints["method"]
+        if method not in allowed_methods:
+            print("error", method)
+            raise ValueError(f"Method {method} is not in allowed_methods {str(allowed_methods)}")
 
-  def fit(self, X, y=None):
-    if self.method == "entropy":
-      return self.fit_entropy(X, self.entropy_value)
-    elif self.method == "class":
-       return self.fit_class(X, y, self.label_value)
-    elif self.method == "all":
-      return self.fit_all(X)
-    
-    
-  def fit_entropy(X, entropy_value:int):
-    train=X
-    entropy = entropy_value
-    area=2**entropy
-    Q=int(train.shape[1]*train.shape[2]/area)
-    mod=train.shape[1]*train.shape[2]%area
-    pixel_set=[]
-    for n in range(area):
-        if n < mod:
-            for i in range(Q+1):
-                pixel_set.append(n)
-        else:
-            for i in range(Q):
-                pixel_set.append(n)
-    # side effect set the pixel_set
-    self.pixel_set = pixel_set
-    return self
-  
-  def fit_class(X, labels, label_value):
-    if label_value not in labels:
-      raise Exception("Label_value not in labels")
+        self.pixel_set = []
+        self.method = method
+        self.entropy_value = entropy_value
+        self.label_value = label_value
 
-    train=X
-    x_train=train[labels==label_value]
-    pixel_list=dict()
-    for n in range(256):
-        pixel_list[n]=0
-    for data in x_train:
-        for raw in data:
-            for pixel in raw:
-                pixel_list[pixel]+=1
-    hist_list=dict()
-    for num in pixel_list:
-        hist_list[num]=pixel_list[num]/len(x_train)
-    hist_num=dict()
-    residual=0
-    for num in hist_list:
-        hist_num[num]=int(np.round(hist_list[num]+residual,0))
-        residual=hist_list[num]+residual-int(np.round(hist_list[num]+residual,0))
-    
-    pixel_set=[]
-    for num in hist_num:
-        repeat=hist_num[num]
-        for n in range(repeat):
-            pixel_set.append(num)
+    def fit(self, X, y=None):
+        if self.method == "entropy":
+            if self.entropy_value not in self._parameter_constraints["entropy_value"]:
+                raise ValueError(
+                    f"Entropy value: {self.entropy_value} is not in {str(self._parameter_constraints['entropy_value'])}")
+            self._fit_entropy(X, self.entropy_value)
+        elif self.method == "class":
+            if self.label_value not in y:
+                raise ValueError(f"label_value: [{self.label_value}] is not in the labels: {str(y)}, change the "
+                                 f"label_value in class definition")
+            self._fit_class(X, y, self.label_value)
+        elif self.method == "all":
+            self._fit_all(X)
+        return self
 
-    # side effect set the pixel_set
-    self.pixel_set = pixel_set
-    return self
-  
-  def fit_all(X):
-    train=X
-    x_train=train
-    pixel_list=dict()
-    for n in range(256):
-        pixel_list[n]=0
-    for data in x_train:
-        for raw in data:
-            for pixel in raw:
-                pixel_list[pixel]+=1
-    hist_list=dict()
-    for num in pixel_list:
-        hist_list[num]=pixel_list[num]/len(x_train)
-    hist_num=dict()
-    residual=0
-    for num in hist_list:
-        hist_num[num]=int(np.round(hist_list[num]+residual,0))
-        residual=hist_list[num]+residual-int(np.round(hist_list[num]+residual,0))
+    def _fit_entropy(self, X, entropy_value: int):
+        train = X
+        entropy = entropy_value
+        area = 2 ** entropy
+        Q = int(train.shape[1] * train.shape[2] / area)
+        mod = train.shape[1] * train.shape[2] % area
+        pixel_set = []
+        for n in range(area):
+            if n < mod:
+                for i in range(Q + 1):
+                    pixel_set.append(n)
+            else:
+                for i in range(Q):
+                    pixel_set.append(n)
+        # side effect set the pixel_set
+        self.pixel_set = pixel_set
 
-    pixel_set=[]
-    for num in hist_num:
-        repeat=hist_num[num]
-        for n in range(repeat):
-            pixel_set.append(num)
+    def _fit_class(self, X, labels, label_value):
+        train = X
+        x_train = train[labels == label_value]
+        pixel_list = dict()
+        for n in range(256):
+            pixel_list[n] = 0
+        for data in x_train:
+            for raw in data:
+                for pixel in raw:
+                    pixel_list[pixel] += 1
+        hist_list = dict()
+        for num in pixel_list:
+            hist_list[num] = pixel_list[num] / len(x_train)
+        hist_num = dict()
+        residual = 0
+        for num in hist_list:
+            hist_num[num] = int(np.round(hist_list[num] + residual, 0))
+            residual = hist_list[num] + residual - int(np.round(hist_list[num] + residual, 0))
 
-    # side effect set the pixel_set
-    self.pixel_set = pixel_set
-    return self
-  
-  # def fit_transform(self, train, labels, method, entropy_value:int, label_value):
-  #   return self.fit(train, labels, method, entropy_value, label_value).transform(train, train, labels)
-    
-  def transform(self, X):
-    x_test=X
-    pixel_set = self.pixel_set
-    equalized_test=np.zeros([x_test.shape[0],x_test.shape[1]*x_test.shape[2]])
-    a=x_test.reshape(-1,x_test.shape[1]*x_test.shape[2])
-    ind=np.argsort(a,axis=1,kind="mergesort")
-    ind2=np.argsort(ind,axis=1)
-    for i in range(len(pixel_set)):
-        equalized_test[ind2==i]=int(pixel_set[i])
-    equalized_test=equalized_test.reshape(x_test.shape[0],x_test.shape[1],x_test.shape[2])
+        pixel_set = []
+        for num in hist_num:
+            repeat = hist_num[num]
+            for n in range(repeat):
+                pixel_set.append(num)
 
-    return equalized_test
+        # side effect set the pixel_set
+        self.pixel_set = pixel_set
+
+    def _fit_all(self, X):
+        train = X
+        x_train = train
+        pixel_list = dict()
+        for n in range(256):
+            pixel_list[n] = 0
+        for data in x_train:
+            for raw in data:
+                for pixel in raw:
+                    pixel_list[pixel] += 1
+        hist_list = dict()
+        for num in pixel_list:
+            hist_list[num] = pixel_list[num] / len(x_train)
+        hist_num = dict()
+        residual = 0
+        for num in hist_list:
+            hist_num[num] = int(np.round(hist_list[num] + residual, 0))
+            residual = hist_list[num] + residual - int(np.round(hist_list[num] + residual, 0))
+
+        pixel_set = []
+        for num in hist_num:
+            repeat = hist_num[num]
+            for n in range(repeat):
+                pixel_set.append(num)
+
+        # side effect set the pixel_set
+        self.pixel_set = pixel_set
+
+    def transform(self, X):
+        x_test = X
+        pixel_set = self.pixel_set
+        equalized_test = np.zeros([x_test.shape[0], x_test.shape[1] * x_test.shape[2]])
+        a = x_test.reshape(-1, x_test.shape[1] * x_test.shape[2])
+        ind = np.argsort(a, axis=1, kind="mergesort")
+        ind2 = np.argsort(ind, axis=1)
+        for i in range(len(pixel_set)):
+            equalized_test[ind2 == i] = int(pixel_set[i])
+        equalized_test = equalized_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2])
+
+        return equalized_test
